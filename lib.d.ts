0bcc79b70eb4dc3b76f3b646e2a9cc47b3bfe785
Brian Takita <brian.takita@gmail.com>
2020-09-19 15:17:00 -0700

    typescript strict checking

--- lib.d.ts
+++ lib.d.ts
@@ -3,5 +3,5 @@
  */
 export declare function __chain(ctx: any, or: any): (...keys: any[]) => any;
 export declare function __chain__or__fn(ctx: any, fn?: () => void): (...keys: any[]) => any;
-export declare function __chain__or__a(ctx: any, a?: any[]): (...keys: any[]) => any;
+export declare function __chain__or__a(ctx: any, a?: never[]): (...keys: any[]) => any;
 export declare function _chain(ctx: any, ...keys: any[]): any;
