6320fe818f89e713d76cb8aa8418c32e90f5b012
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/chain_2.js
+++
@@ -1,6 +0,0 @@
-import { chain_ } from './chain_';
-export function chain_2(obj, or) {
-    return (...keys) => (chain_(obj, ...keys) || or);
-}
-export { chain_2 as __chain };
-//# sourceMappingURL=src/chain_2.js.map
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export * from './chain_';
-export * from './chain_2';
-//# sourceMappingURL=src/index.js.map
