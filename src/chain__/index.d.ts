8666f2b7659609af91b3c177104af2edcbd60047
Brian Takita <brian.takita@gmail.com>
2023-04-04 17:44:03 -0400

    formatting

--- src/chain__/index.d.ts
+++ src/chain__/index.d.ts
@@ -1,7 +1,10 @@
 import type { chain_key__T } from '../chain_'
 export declare function chain__<
 	O extends unknown = unknown
->(obj:object, or:O):(...keys:chain_key__T[])=>O
+>(
+	obj:object,
+	or:O
+):(...keys:chain_key__T[])=>O
 export {
 	chain__ as chain_2,
 	chain__ as __chain,
